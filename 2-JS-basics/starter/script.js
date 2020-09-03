68f40de760210f3fefb3fdcd50fdb0fe1c26c779
Liz Daly <liz7daly@gmail.com>
2020-09-03 10:04:14 +0100

    Add first variables

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,14 @@
-console.log('Hello World!!!')
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);
